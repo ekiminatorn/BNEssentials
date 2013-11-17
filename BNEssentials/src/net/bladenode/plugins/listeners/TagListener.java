07ad44e1f957ea29f731e641ec4866b4935e6fe7
uncleemil <ekstromemil1@hotmail.com>
2013-11-17 02:43:32 +0200

    Temp changes to TagListener class.

--- BNEssentials/src/net/bladenode/plugins/listeners/TagListener.java
+++ BNEssentials/src/net/bladenode/plugins/listeners/TagListener.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 
 import net.bladenode.plugins.BNEssentials;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.kitteh.tag.PlayerReceiveNameTagEvent;
+
 
 public class TagListener implements Listener{
 	//This makes possible to get config in this class!
@@ -21,17 +23,32 @@ public class TagListener implements Listener{
 	
 	@EventHandler
 	public void onNameTag(PlayerReceiveNameTagEvent event) {
-		//Staffs
-		ArrayList<String> list = (ArrayList<String>) plugin.getConfig().getStringList("staffs");
-		if(list.contains(event.getNamedPlayer().getName())){
+		
+		
+		
+		
+		
+		if(event.getNamedPlayer().getName().equals("hunter376398")){
               //If true, sets users tag to blue.
 			event.setTag(ChatColor.BLUE + event.getNamedPlayer().getName());	
-		//Admins
-		ArrayList<String> listadmin = (ArrayList<String>) plugin.getConfig().getStringList("admins");
-		if(listadmin.contains(event.getNamedPlayer().getName())){
-			
-			event.setTag(ChatColor.DARK_RED + event.getNamedPlayer().getName());
 		}
+		if(event.getNamedPlayer().getName().equals("okin336")){
+            //If true, sets users tag to blue.
+			event.setTag(ChatColor.BLUE + event.getNamedPlayer().getName());	
+		}
+		if(event.getNamedPlayer().getName().equals("goth_killer")){
+            //If true, sets users tag to blue.
+			event.setTag(ChatColor.BLUE + event.getNamedPlayer().getName());	
+		}
+		if(event.getNamedPlayer().getName().equalsIgnoreCase("stavrikiosi")){
+            //If true, sets users tag to blue.
+			event.setTag(ChatColor.BLUE + event.getNamedPlayer().getName());	
+		}
+		if(event.getNamedPlayer().getName().equals("Ekstemi")){
+            //If true, sets users tag to blue.
+			event.setTag(ChatColor.DARK_RED + event.getNamedPlayer().getName());	
+		}
+		
 	}	
 	}
-}
+
